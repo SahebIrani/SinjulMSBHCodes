4ffb5fc7dfabf7d1f1aca5f539f3bf11a0b11f04
Saheb Irani <sinjul.msbh@yahoo.com>
2020-04-24 11:14:49 +0430

    Preview 1 ..

---
+++ EFCore5Preview/Controllers/HomeController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using EFCore5Preview.Data;
+using EFCore5Preview.Models;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+
+namespace EFCore5Preview.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController(
+            ILogger<HomeController> logger,
+            ApplicationDbContext applicationDbContext)
+        {
+            _logger = logger;
+            ApplicationDbContext = applicationDbContext;
+        }
+
+        private readonly ILogger<HomeController> _logger;
+        public ApplicationDbContext ApplicationDbContext { get; }
+
+
+        #region Preview1
+        [HttpGet(nameof(EFCore5CreateDbCommandQueryStringAsync))]
+        public async ValueTask<OkResult> EFCore5CreateDbCommandQueryStringAsync(CancellationToken ct = default)
+        {
+            //? Simple way to get generated SQL
+            const string city = "Sari";
+            IQueryable<Address> query = ApplicationDbContext.Addresses.Where(c => c.City == city);
+            string queryString = query.ToQueryString();
+
+            Console.WriteLine(queryString);
+            _logger.LogInformation(queryString);
+
+            //? This will work in simple cases, but the translation to a query string and back to a command loses some information. For example,
+            //? if a transaction is being used then the code above would need to find that transaction and associate itself.
+            DbConnection connection = ApplicationDbContext.Database.GetDbConnection();
+            using (DbCommand command = connection.CreateCommand())
+            {
+                command.CommandText = query.ToQueryString();
+
+                await connection.OpenAsync(ct);
+
+                using (DbDataReader results = await command.ExecuteReaderAsync(ct))
+                {
+                }
+
+                await connection.CloseAsync();
+            }
+
+            //? Instead, EF Core 5.0 introduces CreateDbCommand which creates and configures a DbCommand just as EF does to execute the query. For example:
+            DbConnection connection2 = ApplicationDbContext.Database.GetDbConnection();
+            using (DbCommand command = query.CreateDbCommand())
+            {
+                await connection2.OpenAsync(ct);
+
+                using (DbDataReader results = command.ExecuteReader())
+                {
+                }
+
+                await connection2.CloseAsync();
+            }
+
+            return Ok();
+        }
+
+
+        [HttpGet(nameof(EFCore5DemoAsync))]
+        public async ValueTask<OkResult> EFCore5DemoAsync(CancellationToken ct = default)
+        {
+            //? Simple way to get generated SQL
+            IQueryable<Address> query = ApplicationDbContext.Addresses.Where(c => c.City == "Sari");
+            string queryString = query.ToQueryString();
+
+            Console.WriteLine(queryString);
+            _logger.LogInformation(queryString);
+
+            //? Query translations for more DateTime constructs
+            int count = await ApplicationDbContext.Addresses
+                .CountAsync(c => c.CreateDate >= EF.Functions.DateFromParts(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day), ct);
+
+            //? Query translations for more byte array constructs
+            IEnumerable<Customer> blogs =
+                await ApplicationDbContext.Customers.Where(e => e.Picture.Contains((byte)127)).ToListAsync(ct);
+
+            //? Query translation for Reverse
+            IEnumerable<Customer> getReverse =
+                await ApplicationDbContext.Customers.OrderBy(e => e.CustomerId).Reverse().ToListAsync(ct);
+
+            //? Query translation for bitwise operators
+            //? &(bitwise AND)
+            //? | (bitwise OR)
+            //? ~(bitwise NOT)
+            //? ^(bitwise XOR)
+            //? << (bitwise left shift)
+            //? >> (bitwise right shift)
+            //? >>> (bitwise unsigned right shift)
+            //? &= (bitwise AND assignment)
+            //? |= (bitwise OR assignment)
+            //? ^= (bitwise XOR assignment)
+            //? <<= (bitwise left shift and assignment)
+            //? >>= (bitwise right shift and assignment)
+            //? >>>= (bitwise unsigned right shift and assignment)
+            IEnumerable<Customer> getNegated =
+                await ApplicationDbContext.Customers.Where(o => ~o.CustomerId == -2).ToListAsync(ct);
+
+            return Ok();
+        }
+        #endregion
+
+
+        #region Preview2
+
+        #endregion
+
+
+        #region Preview3
+
+        #endregion
+
+
+
+        public IActionResult Index() => View();
+
+        public IActionResult Privacy() => View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() =>
+            View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ EFCore5Preview/Data/ApplicationDbContext.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace EFCore5Preview.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<ApplicationDbContext> optionsBuilder =
+                new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            optionsBuilder
+                .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=DotNetCore5DemoDb;Trusted_Connection=True;MultipleActiveResultSets=true")
+            ;
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            #region Simple Logging
+            //optionsBuilder.LogTo(Console.WriteLine);
+            //? Set the minimum log level
+            //optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+            //? Filter for only specific events:
+            //? optionsBuilder.LogTo(Console.WriteLine, new[] { CoreEventId.ContextInitialized, RelationalEventId.CommandExecuted });
+            //? Filter for all events in specific categories:
+            //? optionsBuilder.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Name }, LogLevel.Information);
+            //? Use a custom filter over event and level:
+            optionsBuilder.LogTo(Console.WriteLine, (id, level) => id == RelationalEventId.CommandExecuting);
+            #endregion
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //? A new IsRelational method has been added in addition to the existing IsSqlServer, IsSqlite, and IsInMemory. 
+            //? if (Database.IsRelational())
+            //{
+            //	modelBuilder.Entity<Address>().HasNoKey();
+            //}
+            //else Console.WriteLine("Database != IsRelational");
+
+            modelBuilder.Entity<Address>().HasNoKey();
+        }
+    }
+
+    [Keyless]
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public int Zip { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FullName { get; set; }
+        public byte[] Picture { get; set; }
+    }
+}
